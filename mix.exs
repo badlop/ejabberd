2f5ff8bacfb555a959f78baca4f2e27f77af75c9
Badlop <badlop@process-one.net>
2024-09-26 12:07:52 +0200

    Preliminary support for "IQ permission" from XEP-0356 0.4.1 (VERSION 2)

--- mix.exs
+++ mix.exs
@@ -144,7 +144,7 @@ defmodule Ejabberd.MixProject do
      {:p1_utils, "~> 1.0"},
      {:pkix, "~> 1.0"},
      {:stringprep, ">= 1.0.26"},
-     {:xmpp, git: "https://github.com/processone/xmpp.git", ref: "2a54443436dc8a942969f2ef7c5654d5acab7533", override: true},
+     {:xmpp, git: "https://github.com/badlop/xmpp", ref: "3a066e4a6e8bc1260b8a7aa7aee62d87f0eb9006", override: true},
      {:yconf, "~> 1.0"}]
     ++ cond_deps()
   end
