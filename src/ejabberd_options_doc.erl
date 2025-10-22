badlop/ejabberd

commit 5575d4e78e42ca4eb73c74820602a816110365bd

Rename New SQL schema to Multihost, and Default to Singlehost
Right now all names are supported, the previous (obsolete)
and the renamed (preferred). The changes relevant to the usage are:

When preparing configuration, the arguments:
  ./configure --enable-new-sql-schema
  ./configure --enable-multihost-sql-schema

When configuring ejabberd, the toplevel options:
  new_sql_schema: true
  sql_schema_multihost: true

When developing source code, the functions:
  ejabberd_sql:use_new_schema()
  ejabberd_sql:use_multihost_schema()

src/ejabberd_options_doc.erl
@@ -992,23 +992,30 @@ doc() ->
                "bugs. Usually leaving default value of this is option is best, "
                "tweak it only if you know what you are doing. "
                "The default value is '1 minute'.")}},
-     {new_sql_schema,
+     {sql_schema_multihost,
       #{value => "true | false",
+        note => "renamed in 25.xx",
         desc =>
             {?T("Whether to use the "
-                "_`database.md#default-and-new-schemas|new SQL schema`_. "
+                "_`database.md#default-and-new-schemas|multihost SQL schema`_. "
                 "All schemas are located "
                 "at <https://github.com/processone/ejabberd/tree/~s/sql>. "
-                "There are two schemas available. The default legacy schema "
+                "There are two schemas available. The legacy 'singlehost' schema "
                 "stores one XMPP domain into one ejabberd database. "
-                "The 'new' schema can handle several XMPP domains in a "
-                "single ejabberd database. Using this 'new' schema is best when "
+                "The 'multihost' schema can handle several XMPP domains in a "
+                "single ejabberd database. The 'multihost' schema is preferable when "
                 "serving several XMPP domains and/or changing domains from "
                 "time to time. This avoid need to manage several databases and "
                 "handle complex configuration changes. The default depends on "
-                "configuration flag '--enable-new-sql-schema' which is set "
+                "configuration flag '--enable-sql-schema-multihost' which is set "
                 "at compile time."),
              [binary:part(ejabberd_config:version(), {0,5})]}}},
+     {new_sql_schema,
+      #{value => "true | false",
+        note => "obsoleted in 25.xx",
+        desc =>
+            {?T("This option was renamed to _`sql_schema_multihost`_ in ejabberd 25.xx. "
+                "Please update your configuration to use the new option name")}}},
      {update_sql_schema,
       #{value => "true | false",
         note => "updated in 24.06",

.github/workflows/ci.yml
@@ -266,7 +266,7 @@ jobs:
         sudo -u postgres psql ejabberd_test -c "GRANT ALL PRIVILEGES ON ALL
                                                 SEQUENCES IN SCHEMA public
                                                 TO ejabberd_test;"
-        sed -i 's|new_schema, false|new_schema, true|g' test/suite.erl
+        sed -i 's|multihost_schema, false|multihost_schema, true|g' test/suite.erl
     - name: Run DB tests on new schema (mssql, mysql, pgsql)
       run: CT_BACKENDS=mssql,mysql,pgsql make test
       id: ctnewschema

configure.ac
@@ -192,12 +192,20 @@ AC_ARG_ENABLE(mysql,
 esac],[if test "x$mysql" = "x"; then mysql=false; fi])
 
 AC_ARG_ENABLE(new_sql_schema,
-[AS_HELP_STRING([--enable-new-sql-schema],[use new SQL schema by default (default: no)])],
+[AS_HELP_STRING([--enable-new-sql-schema],[obsolete, use --enable-multihost-sql-schema instead (default: no)])],
 [case "${enableval}" in
-  yes) new_sql_schema=true ;;
-  no)  new_sql_schema=false ;;
+  yes) multihost_sql_schema=true ;;
+  no)  multihost_sql_schema=false ;;
   *) AC_MSG_ERROR(bad value ${enableval} for --enable-new-sql-schema) ;;
-esac],[new_sql_schema=false])
+esac],[multihost_sql_schema=false])
+
+AC_ARG_ENABLE(multihost_sql_schema,
+[AS_HELP_STRING([--enable-multihost-sql-schema],[use multihost SQL schema by default (default: no)])],
+[case "${enableval}" in
+  yes) multihost_sql_schema=true ;;
+  no)  multihost_sql_schema=false ;;
+  *) AC_MSG_ERROR(bad value ${enableval} for --enable-multihost-sql-schema) ;;
+esac],[multihost_sql_schema=false])
 
 AC_ARG_ENABLE(odbc,
 [AS_HELP_STRING([--enable-odbc],[enable pure ODBC support (default: no)])],
@@ -318,7 +326,7 @@ esac
 AC_MSG_RESULT([build tool to use (change using --with-rebar): $rebar])
 
 AC_SUBST(roster_gateway_workaround)
-AC_SUBST(new_sql_schema)
+AC_SUBST(multihost_sql_schema)
 AC_SUBST(full_xml)
 AC_SUBST(odbc)
 AC_SUBST(mssql)

include/ejabberd_sql.hrl
@@ -72,4 +72,4 @@
 -record(sql_schema_info,
         {db_type :: pgsql | mysql | sqlite,
          db_version :: any(),
-         new_schema = true :: boolean()}).
+         multihost_schema = true :: boolean()}).

mix.exs
@@ -110,7 +110,7 @@ defmodule Ejabberd.MixProject do
                             {config(:debug), :debug_info},
                             {not config(:debug), {:debug_info, false}},
                             {config(:roster_gateway_workaround), {:d, :ROSTER_GATEWAY_WORKAROUND}},
-                            {config(:new_sql_schema), {:d, :NEW_SQL_SCHEMA}}
+                            {config(:multihost_sql_schema), {:d, :MULTIHOST_SQL_SCHEMA}}
                            ], do:
     option
   end

rebar.config
@@ -144,7 +144,7 @@
             {if_var_false, debug, no_debug_info},
             {if_var_true, debug, debug_info},
             {if_var_true, elixir, {d, 'ELIXIR_ENABLED'}},
-            {if_var_true, new_sql_schema, {d, 'NEW_SQL_SCHEMA'}},
+            {if_var_true, multihost_sql_schema, {d, 'MULTIHOST_SQL_SCHEMA'}},
             {if_var_true, roster_gateway_workaround, {d, 'ROSTER_GATEWAY_WORKAROUND'}},
             {if_var_true, sip, {d, 'SIP'}},
             {if_var_true, stun, {d, 'STUN'}},

src/ejabberd_ctl.erl
@@ -1169,9 +1169,9 @@ get_commands_spec() ->
 			longdesc = "This command is exclusive for the ejabberdctl command-line script, "
 			"don't attempt to execute it using any other API frontend.",
 			note = "added in 24.02",
-			args = [{db_type, string}, {db_version, string}, {new_schema, string}],
+			args = [{db_type, string}, {db_version, string}, {multihost_schema, string}],
                         args_desc = ["Database type: pgsql | mysql | sqlite",
                                      "Your database version: 16.1, 8.2.0...",
-                                     "Use new schema: 0, false, 1 or true"],
+                                     "Use multihost schema: 0, false, 1 or true"],
                         args_example = ["pgsql", "16.1", "true"]}
     ].

src/ejabberd_option.erl
@@ -162,6 +162,7 @@
 -export([sql_prepared_statements/0, sql_prepared_statements/1]).
 -export([sql_query_timeout/0, sql_query_timeout/1]).
 -export([sql_queue_type/0, sql_queue_type/1]).
+-export([sql_schema_multihost/0]).
 -export([sql_server/0, sql_server/1]).
 -export([sql_ssl/0, sql_ssl/1]).
 -export([sql_ssl_cafile/0, sql_ssl_cafile/1]).
@@ -1103,6 +1104,10 @@ sql_queue_type() ->
 -spec sql_queue_type(global | binary()) -> 'file' | 'ram'.
 sql_queue_type(Host) ->
     ejabberd_config:get_option({sql_queue_type, Host}).
+
+-spec sql_schema_multihost() -> boolean().
+sql_schema_multihost() ->
+    ejabberd_config:get_option({sql_schema_multihost, global}).
 
 -spec sql_server() -> binary().
 sql_server() ->

commit f6c66c11479546df944d462131eb19b1fbd7227c

Fix typo introduced in recent commit 5575d4e78

src/ejabberd_options_doc.erl
@@ -1014,8 +1014,8 @@ doc() ->
       #{value => "true | false",
         note => "obsoleted in 25.xx",
         desc =>
-            {?T("This option was renamed to _`sql_schema_multihost`_ in ejabberd 25.xx. "
-                "Please update your configuration to use the new option name")}}},
+            ?T("This option was renamed to _`sql_schema_multihost`_ in ejabberd 25.xx. "
+                "Please update your configuration to use the new option name")}},
      {update_sql_schema,
       #{value => "true | false",
         note => "updated in 24.06",